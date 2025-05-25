334ab5a8391c705f28e6d792e52001f30bac60c1
Marek Mikula <marek@justitpro.com>
2025-05-25 17:09:12 +0200

    feat: bid validation

--- semestralni_prace/app/controllers/item_bids_controller.ts
+++ semestralni_prace/app/controllers/item_bids_controller.ts
@@ -22,6 +22,7 @@ export default class ItemBidsController {
             return response.abort({message: 'Item not found.'}, 404)
         }
 
+        // user cannot bid on his own items
         if (item.userId === user.id) {
             return response.abort({message: 'You cannot bid on your own item.'}, 400)
         }
@@ -33,12 +34,19 @@ export default class ItemBidsController {
 
         const lastBid = await this.itemBidRepository.getLastBid(item.id)
 
+        // user is the last bidder
         if (lastBid && lastBid.userId === user.id) {
             return response.abort({message: 'You have already bid on this item.'}, 400)
         }
 
+        // users bid is too low
         if (lastBid && lastBid.bid >= payload.bid) {
             return response.abort({message: `Your bid is too low. You need to bid at least ${lastBid.bid + 1}`}, 400)
+        }
+
+        // check starting price
+        if (!lastBid && item.startPrice > payload.bid) {
+            return response.abort({message: `Your bid is too low. You need to bid at least ${item.startPrice}`}, 400)
         }
 
         const newBid = await this.itemBidRepository.store({
